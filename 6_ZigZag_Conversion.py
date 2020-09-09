5a96332c28f230230b2cb4bff5b95735f9096928
ArtistVan1 <ytjunjie2006@126.com>
2020-09-09 16:26:15 +0900

    2020/9/9

---
+++ 6_ZigZag_Conversion.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if numRows == 1:
+            return s
+        rows = ['']* min(numRows, len(s))
+        godown = False
+        currow = 0
+        for a in s:
+            rows[currow] += a
+            if currow ==0 or currow == numRows-1:
+                godown = not godown
+            if godown:
+                currow = currow+1
+            else:
+                currow = currow-1
+        return "".join(rows)
