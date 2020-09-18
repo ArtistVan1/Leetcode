def4514c3717814d01e13d6006dd4843d5eef610
ArtistVan1 <ytjunjie2006@126.com>
2020-09-18 21:43:22 +0900

    2020/9/18

---
+++ 14_Longest_Common_Prefix.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if len(strs)==0:
+            return ""
+        min_l=len(strs[0])
+        for w in strs:
+            min_l=min(min_l,len(w))
+        #print(min_l)
+        s = strs[0][0:min_l]
+        #print(s)
+        res=[]
+        for index,item in enumerate(strs):
+            a = 0
+            for i in range(0,min_l):
+                if item[i]==s[i]:
+                    a +=1
+                else:
+                    break
+            res.append(a)
+        res.sort()
+        return s[0:res[0]]
