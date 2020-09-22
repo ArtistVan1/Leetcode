96969d79908ba7095d1b7a1d826c6d3e844d2650
ArtistVan1 <ytjunjie2006@126.com>
2020-09-22 12:19:38 +0900

    2020/9/22

--- 43_Multiply_Strings.py
+++ 43_Multiply_Strings.py
@@ -28,3 +28,29 @@ class Solution:
             del ans[0]
         #连接成字符串
         return ''.join(ans)
+		
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        num1,num2 = num1[::-1], num2[::-1]
+        arr = [0 for i in range(len(num1)+len(num2))]
+        ans = []
+        
+        for i in range(len(num1)):
+            for j in range(len(num2)):
+                arr[i+j] += int(num1[i])*int(num2[j])
+        for i in range(len(arr)):
+            dig = arr[i]%10
+            carry = arr[i]/10
+            if i<len(arr)-1:
+                arr[i+1] +=carry
+            ans.insert(0,str(dig))
+        
+        while ans[0]=='0' and len(ans)>1:
+            del ans[0]
+        return ''.join(ans)
+            
